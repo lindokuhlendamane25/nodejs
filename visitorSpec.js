2e58804a41ece8480df345aafcd13a0483c137cb
lindokuhlendamane25 <lindokuhlengwadla@gmail.com>
2019-08-08 14:19:06 +0200

    Add files via upload

---
+++ visitorSpec.js
@@ -0,0 +1,17 @@
+visit = require("../src/visitor");
+var fs = require('fs');
+describe("spies", function () {
+  var visitor = new Visitor("lindo", "ndamane", 11 / 2 / 2019, 2, "luu");
+  it('should spy on save', function () {
+    var spy = spyOn(visitor, 'save');
+    visitor.save();
+    expect(spy).toHaveBeenCalled();
+  })
+})
+describe("read", function () {
+  var visitor = new Visitor("Sam", "Silika", 24 / 07 / 2019, 2, "buhle");
+  it("should read content", function () {
+    var result = visitor.load(1);
+    expect(result).not.toBeNull();
+  })
+})

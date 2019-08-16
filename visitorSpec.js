4b63a1629f7e52f1b573ae19cddd1ba988b4ba81
lindokuhlendamane25 <lindokuhlengwadla@gmail.com>
2019-08-16 11:18:55 +0200

    Update visitorSpec.js

--- visitorSpec.js
+++ visitorSpec.js
@@ -1,15 +1,12 @@
-var fs = require('fs');
-var visit = require("./visitor");
-describe("spies", function () {
-  var visitor = new Visitor("lindo", "ndamane", 11 / 2 / 2019, 2, "luu");
-  it('should spy on save', function () {
-    var spy = spyOn(visitor, 'save');
-    visitor.save();
-    expect(spy).toHaveBeenCalled();
-  })
-})
+visit = require('../visitor');
+var visitor = new Visitor("Lindo", "Ndamane", 11 / 2 / 2019, 2, "luu");
+describe("save", function () {
+  it('should create a json file', function () {
+    expect(visitor.save()).toContain("saved");
+  });
+});
+
 describe("read", function () {
-  var visitor = new Visitor("Sam", "Silika", 24 / 07 / 2019, 2, "buhle");
   it("should read content", function () {
     var result = visitor.load(1);
     expect(result).not.toBeNull();
